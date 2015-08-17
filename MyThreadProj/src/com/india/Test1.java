6b43e362a304d87eda63fcf851a735c14cf3ec57
Kabita Kumari <kabitakumari@users.noreply.github.com>
2015-08-17 08:19:31 +0530

    Thread First Example

--- MyThreadProj/src/com/india/Test1.java
+++ MyThreadProj/src/com/india/Test1.java
@@ -1,5 +1,34 @@
 package com.india;
 
-public class Test1 {
+public class Test1{
+public static void main(String args[]){
+Hello h = new Hello();
+h.show();
+h.display();
+}
+}
 
+class Hello{
+void show(){
+Thread th=Thread.currentThread();
+for(int i=1;i<=10;i++){
+System.out.println("SHOW\t"+i+"\t"+th.getName());
+try{
+Thread.sleep(1000);
+}catch(Exception e){
+e.printStackTrace();
+}
+}
+}
+void display(){
+Thread th=Thread.currentThread();
+for(int i=11;i<=20;i++){
+System.out.println("DISP\t"+i+"\t"+th.getName());
+try{
+Thread.sleep(1000);
+}catch(Exception e){
+e.printStackTrace();
+}
+}
+}
 }
